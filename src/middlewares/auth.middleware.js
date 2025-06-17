49f5bb3175211d652b75431b71c0b38ae1f88662
Muhammad Shazil <shazilshabbir@gmail.com>
2025-06-18 04:09:19 +0500

    Refactor auth middleware and enhance subscription model comments

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,30 +1,32 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import  Jwt  from "jsonwebtoken";
-import { User } from "../models/user.model.js"
+import Jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
 
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
 
-export const verifyJWT =  asyncHandler(async(req,res,next)=>{
-    try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    
-        if (!token) {
-            throw new ApiError(401, "Unauthorized request")
-        }
-    
-        const decodedToken = Jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-           
-        if (!user) {
-            // Next_Video: discuss about frontend
-            throw new ApiError(401,"Invalid Access Token")
-        }
-        
-        req.user = user;
-        next()
-    } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token")
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
     }
 
-})
+    const decodedToken = Jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      // Next_Video: discuss about frontend
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,11 +2,11 @@ import mongoose, {Schema} from "mongoose";
 
 const subscriptionSchema = new Schema({
     subscriber:{
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,//one who is subscribing
         ref: "User"
     },
     channel:{
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,//the channel being subscribed to
         ref: "User"
     }
 },{timestamps:true})
