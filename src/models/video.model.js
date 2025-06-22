fb4936fdc2128c1fa232499158e635a7ff38a19a
Muhammad Shazil <shazilshabbir@gmail.com>
2025-06-23 03:04:36 +0500

    Add models for comments, likes, playlists, and tweets with appropriate schemas

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -6,40 +6,38 @@ const videoSchema = new Schema(
       type: String, // cloudinary url
       required: true,
     },
-    thumbnail:{
-        type: String, // cloudinary url
-        required: true,
+    thumbnail: {
+      type: String, // cloudinary url
+      required: true,
     },
-    title:{
-        type: String,
-        required: true,
+    title: {
+      type: String,
+      required: true,
     },
-    description:{
-        type: String,
-        required: true,
+    description: {
+      type: String,
+      required: true,
     },
-    duration:{
-        type: Number, 
-        required: true,
+    duration: {
+      type: Number,
+      required: true,
     },
-    views:{
-        type:Number,
-        dafault: 0,
+    views: {
+      type: Number,
+      dafault: 0,
     },
-    isPublished:{
-        type: Boolean,
-        default: true
+    isPublished: {
+      type: Boolean,
+      default: true,
     },
-    owner:{
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-
-
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   { timestamps: true }
 );
 
-videoSchema.plugin(mongooseAggregatePaginate)
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
